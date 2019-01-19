bf3c3b1860738c1c855bae0e1a7055062241d7dd
addiedavis <adeline.davis19@gmail.com>
2019-01-19 13:46:00 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+## Virtual Pet Project
+- create a VirtualPet class
+- create a VirtualPetApp class with a main method
---
+++ src/test/java/virtualpet/virtualPetTest.java
@@ -0,0 +1,12 @@
+package virtualpet;
+
+import org.junit.Test;
+
+public class virtualPetTest {
+	virtualPet underTest = new virtualPet();
+
+	@Test
+	public void shouldAskToNamePet() {
+	}
+
+}
